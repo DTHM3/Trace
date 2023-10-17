57fc3bcb98fe2f68010e20b3135f027b9d5bde39
DTHM3 <trippm077@gmail.com>
2023-10-17 16:22:44 -0400

    Sheets API implementation basics

--- components/loginView.js
+++ components/loginView.js
@@ -3,15 +3,32 @@ import { GoogleSignin } from '@react-native-google-signin/google-signin';
 import React, { useState, useEffect } from 'react';
 import auth from '@react-native-firebase/auth';
 
+import {
+  GDrive,
+  MimeTypes
+} from "@robinbobin/react-native-google-drive-api-wrapper";
+
 const LoginView = () => {
   const [initializing, setInitializing] = useState(true);
   const [user, setUser] = useState();
+  const [signedIn, setSignedIn] = useState(false); // integer state
 
   // Handle user state changes
   function onAuthStateChanged(user) {
     setUser(user);
     if (initializing) setInitializing(false);
   }
+
+  signOut = async () => {
+    try {
+      await GoogleSignin.revokeAccess();
+      setUser(null);
+      // Google Account disconnected from your app.
+      // Perform clean-up actions, such as deleting data associated with the disconnected account.
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   useEffect(() => {
     const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
@@ -42,29 +59,57 @@ const LoginView = () => {
     return auth().signInWithCredential(googleCredential);
   }
 
-  if (initializing) return null;
+  async function onDriveButtonPress() {
+    const gdrive = new GDrive();
+    gdrive.accessToken = (await GoogleSignin.getTokens()).accessToken;
 
-  if (!user) {
-    return (
-      
+    console.log(await gdrive.files.list());
+
+    const id = (await gdrive.files.newMultipartUploader()
+      .setData([1, 2, 3, 4, 5], MimeTypes.BINARY)
+      .setRequestBody({
+        name: "multipart_bin"
+      })
+      .execute()
+    ).id;
+
+    console.log(await gdrive.files.getBinary(id));
+  }
+
+  function useForceUpdate(){
+    setSignedIn(!signedIn); // update the state to force render
+  }
+
+  const RetComponent = () => {
+    if (!user) {
+      return (
       <View>
         <Button
-          title="Google Sign-In"
-          onPress={() => onGoogleButtonPress().then(() => console.log('Signed in with Google!'))}
+        title="Google Sign-In"
+        onPress={() => onGoogleButtonPress().then(() => {useForceUpdate(); console.log('Signed in with Google!')})}
         />
         <Text>Login</Text>
       </View>
-    )
+      )
+    }
+    return (<View><Button
+        title="Drive"
+        onPress={() => onDriveButtonPress().then(() => {console.log('Done loading Drive!')})}
+      />
+      <Button
+        title="Sign Out"
+        onPress={() => signOut().then(() => {useForceUpdate(); console.log('Signed Out');})}
+      />
+      <Text>Welcome {user.email}</Text>
+    </View>)
   }
+
+  if (initializing) return null;
   return (
     <View>
-        <Button
-          title="Google Sign-In"
-          onPress={() => onGoogleButtonPress().then(() => console.log('Signed in with Google!'))}
-        />
-        <Text>Welcome {user.email}</Text>
-      </View>
-  );
+      <RetComponent />
+    </View>
+  )
 }
 
 export default LoginView;
